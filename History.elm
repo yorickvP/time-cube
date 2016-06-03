9d55f4db8be77f8438aec3a9addca5016a46334f
Yorick van Pelt <yorickvanpelt@gmail.com>
2016-06-03 23:23:12 +0200

    Creating github pages

---
+++ History.elm
@@ -0,0 +1,232 @@
+port module History exposing (Model, SerialModel, serialize, deserialize, update, view, Msg, empty, addTime, getStats)
+
+import Time exposing (Time, second)
+import Html exposing (Html, text, ul, li, a, span, section)
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (class, attribute, href, title)
+import List
+import String exposing (slice, padRight)
+import Date
+import Date.Format
+import Scrambles
+import Util exposing ((<$>))
+
+
+
+-- MODEL
+mapintToFlag : Int -> Flag
+mapintToFlag i = case i of
+  1 -> Penalty2
+  2 -> DNF
+  _ -> None
+mapFlagtoint : Flag -> Int
+mapFlagtoint f = case f of
+  None -> 0
+  Penalty2 -> 1
+  DNF -> 2
+
+type alias OldTime = {
+  time : Time,
+  startTime : Time,
+  comment : String,
+  flag : Flag,
+  scrambleType : Scrambles.Model
+}
+
+type alias SerialOldTime = {
+  time : Time,
+  startTime : Time,
+  comment : String,
+  flag : Int,
+  scrambleType : Scrambles.Model -- load from scrambles array?
+}
+
+type Flag = None | Penalty2 | DNF
+type alias OldTimeID = Int
+type alias Model = List OldTime
+type alias SerialModel = List SerialOldTime
+
+port alert : String -> Cmd msg
+
+
+serializeTime : OldTime -> SerialOldTime
+serializeTime t = {t | flag = mapFlagtoint t.flag}
+deserializeTime : SerialOldTime -> OldTime
+deserializeTime t = {t | flag = mapintToFlag t.flag}
+
+
+serialize : Model -> SerialModel
+serialize = List.map serializeTime
+
+
+deserialize : SerialModel -> Model
+deserialize = List.map deserializeTime
+
+empty : SerialModel
+empty = []
+
+-- UPDATE
+type Msg
+  = ToggleFlag Flag OldTimeID
+  | DeleteTime OldTimeID
+  | GetScramble OldTimeID
+
+
+setFlag : Flag -> OldTimeID -> List OldTime -> List OldTime
+setFlag fl id otm =
+  let
+    m : OldTimeID -> OldTime -> OldTime
+    m i oldtime =
+      if i == id then
+        {oldtime | flag = (if oldtime.flag == fl then None else fl)}
+      else oldtime
+  in
+    List.indexedMap m otm
+
+rmTime : OldTimeID -> List OldTime -> List OldTime
+rmTime id times = (List.take id times) ++ (List.drop (id + 1) times)
+
+lookupTime : OldTimeID -> List OldTime -> Maybe OldTime
+lookupTime id list = snd <$> List.head (List.indexedMap (,) list |> List.filter (fst >> ((==) id)))
+
+addTime : {time : Time, startTime: Time, scramble : Scrambles.Model, inspectTime : Time} -> Model -> Model
+addTime tm =
+    let
+      penaltyflag =
+        if tm.inspectTime > 17 * second then DNF
+        else if tm.inspectTime > 15 * second then Penalty2
+        else None
+      ot = OldTime tm.time tm.startTime "" penaltyflag tm.scramble
+    in (::) ot
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    ToggleFlag flag id ->
+        (setFlag flag id model, Cmd.none)
+    DeleteTime id ->
+        (rmTime id model, Cmd.none)
+    GetScramble id ->
+        (model, alert <| Maybe.withDefault "" <| (Scrambles.toString << (.scrambleType)) <$> (lookupTime id model))
+
+
+type alias Stats =
+  { total : Int
+  , totalFin : Int
+  , best : Maybe Time
+  , worst : Maybe Time
+  , average : Maybe (Time, Float) -- SD
+  , mean : Maybe Time
+  , averages : List (Int, (Time, Float), (Time, Float))
+  }
+
+trimOutlying : List comparable -> List comparable
+trimOutlying list =
+    let
+        length = List.length list
+        sorted = List.sort list
+        trim = ceiling (toFloat length / 20.0)
+        trimmed = List.take (length - (2 * trim)) (List.drop trim sorted)
+    in trimmed
+
+average : List Time -> Time -- possible div/0
+average times = List.sum times / (toFloat <| List.length times)
+
+getAvgSD : List Time -> Maybe (Time, Float)
+getAvgSD times =
+    if List.length times < 3 then Nothing
+    else let
+        trimmed = trimOutlying times
+        avg = average trimmed
+        variance = average <| List.map (\x -> ((abs (x - avg)) / Time.second) ^ 2) trimmed
+        sd = sqrt variance
+    in Just (avg, sd)
+
+mapMin : (a -> comparable) -> List a -> Maybe a
+mapMin fun = List.head << List.sortBy fun
+
+mapWithN : Int -> (List x -> y) -> List x -> List y
+mapWithN n fun list =
+  let
+    nl = List.take n list
+  in
+    if (List.length nl) < n then []
+    else (fun nl) :: (mapWithN n fun (List.drop 1 list))
+
+getMaybes : List (Maybe a) -> List a
+getMaybes = List.filterMap identity
+
+-- impossible to do this generically for now
+maybe_lift2 : (e -> f -> g) -> Maybe e -> Maybe f -> Maybe g
+maybe_lift2 fun a b = a `Maybe.andThen` (\firsta -> b `Maybe.andThen` (Just << fun firsta))
+
+getStats : Model -> Stats
+getStats model = let
+  notDNF = List.filter (.flag >> (/=) DNF) model
+  times = List.map getTimeWithPenalty notDNF
+  getAverage : Int -> Maybe (Int, (Time, Float))
+  getAverage n = if List.length times < n then Nothing else
+    Maybe.map ((,) n) (getAvgSD (List.take n times))
+  getBestAvg : Int -> Maybe (Int, (Time, Float))
+  getBestAvg n = if List.length times < n then Nothing else
+    Maybe.map ((,) n) <|
+    mapMin (fst) <| getMaybes <| mapWithN n getAvgSD times
+  combineTuples (x, a) (y, b) = (x, a, b)
+ in
+  { total = List.length model
+  , totalFin = List.length times
+  , best = List.minimum <| times
+  , worst = List.maximum <| times
+  , mean =
+        if List.isEmpty times
+            then Nothing
+            else Just <| average times
+  , average = getAvgSD times
+  , averages = getMaybes
+    [ maybe_lift2 combineTuples (getAverage 5) (getBestAvg 5)
+    , maybe_lift2 combineTuples (getAverage 12) (getBestAvg 12)
+    , maybe_lift2 combineTuples (getAverage 50) (getBestAvg 50)
+    , maybe_lift2 combineTuples (getAverage 100) (getBestAvg 100)
+    ]
+  }
+
+-- view
+
+
+floatPart : Float -> Float
+floatPart i = i - (toFloat <| truncate i)
+
+
+pretty : Time -> String
+pretty time =
+  let x = (toFloat <| floor (time / 10)) / 100
+  in (toString <| truncate x) ++ "." ++ (padRight 2 '0' <| slice 2 4 <| toString <| floatPart x)
+
+
+getTimeWithPenalty : OldTime -> Time
+getTimeWithPenalty time = time.time + if time.flag == Penalty2 then second * 2 else 0
+
+view : Model -> Html Msg
+view oldTimes = let
+  flagsToClassName : OldTime -> String
+  flagsToClassName t = case t.flag of
+    None -> ""
+    Penalty2 -> "penalty2"
+    DNF -> "dnf"
+  flagElm : Flag -> OldTimeID -> OldTime -> String -> Html Msg
+  flagElm fl i tm txt =
+    a [ class <| "flag" ++ if tm.flag == fl then " active" else ""
+      , href "#", onClick (ToggleFlag fl i)] [text txt]
+  mapTime : OldTimeID -> OldTime -> Html Msg
+  mapTime i t = li
+    [ attribute "data-id" (toString i)
+    , class (flagsToClassName t)
+    , title <| Date.Format.format "%d-%m-%Y %H:%M:%S" (Date.fromTime (t.startTime + t.time))
+    ]
+    [ span [class "textlabel"] [text <| pretty <| getTimeWithPenalty t]
+    , flagElm Penalty2 i t "+2"
+    , flagElm DNF i t "DNF"
+    , a [class "getscramble", href "#", onClick (GetScramble i)] [text "scramble?"]
+    , a [class "delete", href "#", onClick (DeleteTime i)] [text "x"]
+    ]
+  in
+  section [ class ("oldtimes") ] [ ul [] (List.reverse <| List.indexedMap mapTime oldTimes) ]
---
+++ timer.elm
@@ -0,0 +1,230 @@
+port module Timer exposing (main, setStorage, scrollBottom, scrambleReq, scrambleRes)
+import Html exposing (Html, button, div, text, h1, ul, li, a, span, header, section, select, optgroup, option)
+import Html.App as Html
+import Html.Lazy as Html
+import Html.Attributes exposing (class, attribute, href, title, value, selected)
+import Time exposing (Time, second)
+import Task
+import Keyboard
+
+import Scrambles
+import History
+
+import Util exposing ((<$>), pretty, toPrecision)
+
+port setStorage : SerialModel -> Cmd msg
+port scrollBottom : String -> Cmd msg
+
+
+port scrambleReq : Scrambles.ScrType -> Cmd msg
+port scrambleRes : (String -> msg) -> Sub msg
+
+main : Program (Maybe SerialModel)
+main =
+  Html.programWithFlags
+  { init = init
+  , view = view
+  , update = update
+  , subscriptions = subscriptions }
+
+
+-- MODEL
+
+-- waiting is a hack for the time between keyup and keydown, really
+type TimerState = Stopped | Running | Inspecting | Waiting
+type alias SerialModel = {
+    oldTimes : History.SerialModel
+  , scrType : Scrambles.SerialModel
+}
+
+serialize : Model -> SerialModel
+serialize m = SerialModel (History.serialize m.history) (Scrambles.serialize m.scramble)
+
+
+type alias Model = {
+  time : Time,
+  startTime : Time,
+  state : TimerState,
+  totalInspection : Time,
+  scramble : Scrambles.Model,
+  history : History.Model,
+  inspectionTime : Time
+}
+
+
+deserialize : SerialModel -> Model
+deserialize st = Model 0 0 Stopped (15 * second) (Scrambles.deserialize st.scrType) (History.deserialize st.oldTimes) 0
+
+empty : SerialModel
+empty = SerialModel History.empty Scrambles.empty
+
+init : Maybe SerialModel -> (Model, Cmd Msg)
+init local_storage =
+  let
+    stored : SerialModel
+    stored = Maybe.withDefault empty local_storage
+  in
+    withGetScramble (deserialize stored, Cmd.none)
+
+withCmd : (Model -> Cmd Msg) -> (( Model, Cmd Msg ) -> ( Model, Cmd Msg ))
+withCmd makeCmd (model, cmds) = (model, Cmd.batch [makeCmd model, cmds])
+
+withSetStorage : ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
+withSetStorage = withCmd (setStorage << serialize)
+
+withGetScramble : (Model, Cmd Msg) -> (Model, Cmd Msg)
+withGetScramble = withCmd (\model -> scrambleReq model.scramble.scrtype )
+
+
+-- UPDATE
+type UpDown = Up | Down
+
+type Msg
+  = NoOp
+  | Tick Time
+  | StartTime Time
+  | Space UpDown
+  | HistoryMsg History.Msg
+  | ScrambleMsg Scrambles.Msg
+
+getCurTime : (Time -> Msg) -> Cmd Msg
+getCurTime m = Task.perform m m Time.now
+
+addOldTime : Model -> Model
+addOldTime m =
+    updateHistory m <| History.addTime {
+      time = m.time,
+      startTime = m.startTime,
+      scramble = m.scramble,
+      inspectTime = m.inspectionTime
+    } m.history
+
+scrollOldTimes : Cmd Msg
+scrollOldTimes = scrollBottom ".oldtimes"
+
+const : a -> b -> a
+const a b = a
+
+updateHistory : Model -> History.Model -> Model
+updateHistory m newhist = {m | history = newhist}
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  let
+    upM m = (m, Cmd.none)
+    donothing = upM model
+  in
+  case msg of
+    NoOp -> donothing
+    Tick t ->
+      upM { model | time = t - model.startTime }
+    StartTime t ->
+      upM { model | time = 0, startTime = t, state = Inspecting}
+    Space Down ->
+      case model.state of
+        Running ->
+          withGetScramble <| withSetStorage <| (addOldTime { model | state = Waiting }, scrollOldTimes)
+        Inspecting ->
+          upM { model | state = Running, startTime = model.time + model.startTime, time = 0, inspectionTime = model.time }
+        _ -> donothing
+    Space Up ->
+      case model.state of
+        Stopped -> (model, getCurTime StartTime)
+        Waiting -> upM {model | state = Stopped }
+        _ -> donothing
+    HistoryMsg msg ->
+      let
+        (newhist, histmsg) = History.update msg model.history 
+      in
+        withSetStorage <| ({model | history = newhist}, Cmd.map HistoryMsg histmsg)
+    ScrambleMsg msg ->
+        let
+          (newscrmbl, shouldupdate) = Scrambles.update msg model.scramble
+        in
+            if shouldupdate then
+                withSetStorage <| withGetScramble <| upM {model | scramble = newscrmbl}
+            else upM {model | scramble = newscrmbl}
+
+keyupdown : UpDown -> Int -> Msg
+keyupdown updown keycode =
+  if keycode == 32
+    then Space updown
+    else NoOp
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions m =
+ let
+  shouldTick = ((m.state == Running) || (m.state == Inspecting))
+ in
+  Sub.batch
+    [ scrambleRes (ScrambleMsg << Scrambles.newscramble)
+    , Keyboard.downs (keyupdown Down)
+    , Keyboard.ups (keyupdown Up)
+    , (if shouldTick then
+        Time.every (second / 100) Tick
+      else Sub.none)
+    ]
+-- VIEW
+
+getRunTime : Model -> Float
+getRunTime m = (toFloat <| round (m.time / 10)) / 100
+
+getInspectTime : Model -> String
+getInspectTime m =
+  let
+    curtime = m.totalInspection - m.time
+  in
+    if curtime < (-2 * second) then "DNF"
+    else if curtime < 0 then "+2"
+    else toString <| ceiling (curtime / 1000)
+
+viewStats : History.Model -> Html Msg
+viewStats history =
+  let
+    renderTimeSD : (Time, Float) -> String
+    renderTimeSD (t, sd) = (pretty t) ++ " (σ=" ++ (toPrecision 2 sd) ++ ")"
+    st = History.getStats history
+    stat : String -> String -> Html Msg
+    stat label content = div [ class "stat"] [
+        span [class "stat_left"] [text label], span [] [text content]]
+    statSec : String -> List (String, String) -> Html Msg 
+    statSec head rows = div [class "stat_group"]
+      ([ div [ class "stat_hdr" ] [text head] ] ++
+        List.map (uncurry stat) rows)
+    mapAvg : (Int, (Time, Float), (Time, Float)) -> Html Msg
+    mapAvg (avgn, cur, best) =
+      statSec ("avg" ++ toString avgn)
+      [ ("current", renderTimeSD cur)
+      , ("best", renderTimeSD best)]
+  in
+  section [ class ("stats") ]
+  ([ header [] [text <| "stats (" ++ (toString st.totalFin) ++ "/" ++ (toString st.total) ++ ")"]
+  , statSec "times"
+    (List.filterMap identity
+    [ ((,) "best" << pretty) <$> st.best
+    , ((,) "worst" << pretty) <$> st.worst
+    , ((,) "average" << renderTimeSD) <$> st.average
+    , ((,) "mean" << pretty) <$> st.mean
+    ])
+  ] ++ (List.map mapAvg st.averages))
+
+view : Model -> Html Msg
+view model = let
+  timefmt = case model.state of
+    Stopped -> if model.startTime == 0 then "Ready" else pretty model.time
+    Inspecting -> getInspectTime model
+    _ -> pretty model.time
+  inspectingpenalty = model.state == Inspecting && model.time > 15 * second
+  stateToClassName = case model.state of
+    Stopped -> "stopped"
+    Inspecting -> "inspecting" ++ (if inspectingpenalty then " penalty" else "")
+    Running -> "running"
+    Waiting -> "stopped"
+  in
+  div [class "container"]
+  [ Html.map ScrambleMsg <| Html.lazy Scrambles.view model.scramble
+  , section [ class ("timerstate " ++ stateToClassName) ] [ h1 [] [ text timefmt ] ]
+  , Html.map HistoryMsg  <| Html.lazy History.view model.history
+  , Html.lazy viewStats model.history
+  ]
